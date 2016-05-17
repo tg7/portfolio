2a08a1e550a1eec61a59a3c67d6338bc6db33ceb
Todd Germaine <trgermaine@gmail.com>
2016-05-17 11:52:08 -0400

    update files

--- week-3-game/assets/javascript/game.js
+++ week-3-game/assets/javascript/game.js
@@ -1,6 +1,41 @@
-window.onload = function () {
+window.onload = function () {.
 
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'z'];
 
 }
 
+//On-key up function
+
+	document.onkeyup = function(event) {
+		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	}
+	
+	function loadWords() {
+		var word = '';
+
+		// Words (Add Math.Random to word var)
+
+		var word = ;
+
+		var guess = [ ];
+
+		var lives = ;
+
+		var counter = ;
+
+		// Spaces In Word
+
+		var spaces = ?;
+
+
+		// Grab Elements From The Array 
+
+		document.getElementById('alphabet');
+	}
+
+		// For Loop for Alphabet
+
+		for (var i = 0; i < alphabet.length; i++) {
+
+
+		}
