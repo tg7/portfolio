6d1df610816d51841f21c1eb6e4fe1156565415c
Todd Germaine <trgermaine@gmail.com>
2016-05-20 10:55:50 -0400

    update files

--- week-3-game/assets/javascript/game.js
+++ week-3-game/assets/javascript/game.js
@@ -1,33 +1,79 @@
-//window.onload = function () {.
 
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'z'];
+	var wins = 0;
+	var losses = 0;
+	var guessesLeft = 10;
+
+	var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
+	var answer = ['b', 'r', 'a', 'v', 'e', 's'];
+	var boxes =  ['_', '_', '_', '_', '_', '_'];
 
-//On-key up function
+	var boxElement = document.getElementById('boxes');
+	console.log('boxElement', boxElement);
 
-		var wins = 0;
-		var losses = 0;
-		var guessesLeft = 0;
-	
+	var remaining = document.getElementById('remaining');
+	console.log('remaining', remaining);
+
+
 	document.onkeyup = function(event) {
-		if (guessesLeft >= 0) {
-			var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-			// var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+				var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-	}
-	
-	function loadWords() {
-		var word = '';
-		var guesses = ['_', '_', '_', '_', '_', '_'];
+				console.log('userGuess', userGuess);
 
-		var word = ['b', 'r', 'a', 'v', 'e', 's'];
-		for (var i = 0; i< word.length;i++) {
-			//check word[i] == userGuess
+				var instr = document.getElementById('instructions');
+				instr.innerHTML = "Game started, press a letter!";
 
-			//then show the letter on the page
-			
+		// var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+		//function loadWords() {
+
+	// For Loop
+
+	for (var i = 0; i<answer.length;i++) {
+		if (userGuess == answer[i]) {
+			boxes[i] = answer[i];
 		}
-
-
 	}
+
+	if (answer.indexOf(userGuess) >= 0) {
+		//true
+	} else {
+		guessesLeft--;
+		//if guessesLeft is 0 
+		//losses-- ? 
+		//reset
+	}
+
+	//how to win?
+	if (answer.indexOf('_') == -1) {
+		//then u win
+	}
+
+	boxElement.innerHTML = boxes;
+	remaining.innerHTML = guessesLeft;
+
+}
+
+// function reset 	
+		function reset() {
+		guessesLeft = 10;
+		answer = 0;// fix answer here
+	}
+
+
+		// Pick a word from the word list and randomize using Math.random //
+
+
+
+	
+
+
+		// if (guesses === 'b' || 'r' || 'a' || 'v' || 'e' || 's') {
+		// 		alert('This is working');
+
+		// 	};
+		
+	
+				
