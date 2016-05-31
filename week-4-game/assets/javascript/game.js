3810ad1fbacc993f2b5339d264668c2afd963419
Todd Germaine <trgermaine@gmail.com>
2016-05-31 18:32:54 -0400

    Resubmitting HW4

--- week-4-game/assets/javascript/game.js
+++ week-4-game/assets/javascript/game.js
@@ -9,6 +9,16 @@ function reset(){
 	isRivalPicked = false;
 };
 
+function resetGame() {
+	window.location.href="index.html";
+
+}
+
+function winGame() {
+	window.location.href="win.html";
+
+}
+
 // Audio
 
 var audio = new Audio('assets/tmntThemeSong.mp3');
@@ -25,17 +35,17 @@ var characters = {
 	Leonardo: {
 		name: 'Leonardo',
 		life: 100,
-		attack: 22		
+		attack: 18		
 	},
 	Raphael: {
 		name: 'Raphael',
 		life: 120,
-		attack: 10
+		attack: 5
 	}, 
 	Michelangelo: {
 		name: 'Michelangelo',
 		life: 75,
-		attack: 12
+		attack: 8
 	}
 };
 
@@ -43,8 +53,9 @@ var characters = {
 
 var isCharacterPicked = false;
 var isRivalPicked = false;
-
+var characterChosen = "";
 var character;
+var charactersLife;
 var rival;
 
 // Makes Attack Button Clickable & Append Text To Defender Div
@@ -55,26 +66,43 @@ function rmRival () {
 
 	};
 
-
 // On click function for Button & Attacks
 
 	$('.attack').on('click', function() {
-
+	
 
 	$('.attackMessage').html('You\'ve attacked for ' + character.attack + '<br> You\'ve been attacked for ' + rival.attack);
 
-	character.life = character.life - rival.attack;
-	rival.life = rival.life - character.attack;
+	// function replacetext()
 
-	if (rival.life <= 0) {		
+	//  $("#name").replaceWith($('<textarea>').attr({ id: 'name', value: $('#name').text() }));
+
+	character.life = character.life - rival.attack;
+
+	rival.life = rival.life - character.attack;
+	console.log("Life is now: " + characters.Donatello.life);
+	$('#donLife').html(characters.Donatello.life);
+	$('#leoLife').html(characters.Leonardo.life);
+	$('#michLife').html(characters.Michelangelo.life);
+	$('#raphLife').html(characters.Raphael.life);
+	// Adjusts Remaining Life
+
+	
+	 if (rival.life <= 0) {	
+
 		setTimeout(rmRival, 2000);
 		$('#' + rival.name).html(rival.name + ' has been defeated');
 		reset();
-	}
+
+	} else if (character.life <=0) {
+		alert('You Lose!');
+		resetGame();
+
+	};
+
 
 	// $('.health').html('Health: ' + character.life);
 	// $('.health2').html('Health: ' + rival.life)
-	
 	
 	});
 
@@ -83,14 +111,20 @@ function rmRival () {
 
 $('.boxes').on('click', function(fight) {
 	if (isCharacterPicked == false) {
+		console.log(characters.Donatello.life);
+		console.log(characters.life);
 		$(this).appendTo('#characters');
 		var id = ($(this).attr('id'));
 
 		//character = the character that was clicked
 		character = characters[id];
-		console.log('character is' , character);
-
+		console.log('character id is' , character);
+		console.log('character is' , character.name);
+		characterChosen = character.name;
+		charactersLife = character.life;
+		console.log('the characters life is ', charactersLife)
 		isCharacterPicked = true;
+
 	} else if (isRivalPicked == false && isCharacterPicked == true) {
 		$(this).appendTo('#rival');
 			isRivalPicked = true;
@@ -98,6 +132,12 @@ $('.boxes').on('click', function(fight) {
 			var id = ($(this).attr('id'));
 			rival = characters[id];
 	} 
+
+	if (isRivalPicked == true && isCharacterPicked == false) {
+		alert('You Win!');
+		winGame();
+	};
+
 });
 
 }
