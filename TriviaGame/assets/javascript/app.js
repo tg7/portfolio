c7536d7a8a96da1e509ab9a4ffe4a51a2f43c8f6
Todd Germaine <trgermaine@gmail.com>
2016-06-04 11:01:53 -0400

    added TriviaGame HW

---
+++ TriviaGame/assets/javascript/app.js
@@ -0,0 +1,48 @@
+$( document ).ready(function() {
+        console.log( "document loaded" );
+  
+
+// Variables
+	
+	var
+	var 
+
+// Starts The Game On Click
+	$('#beginGame').on('click', function() {
+
+// Starts Interval Timer
+
+	setInterval(timeClock, 30000) {
+
+	$('#timer').append(timeClock);
+
+
+	};
+
+
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
